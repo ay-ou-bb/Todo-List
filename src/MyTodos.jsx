a860e683d200f2dc9cf14903ddba4aaaea5bc364
ay-ou-bb <ayoubbo3333@gmail.com>
2026-01-11 16:00:14 +0100

    feat: add form of todo

--- src/MyTodos.jsx
+++ src/MyTodos.jsx
@@ -5,16 +5,16 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
+import Todos from "./Todos.jsx"
 
 export default function MyTodos() {
   return (
     <React.Fragment>
       <Container maxWidth="md">
-          <Card sx={{ minWidth: 500 }}>
+          <Card sx={{ minWidth: 500,display:"flex" ,alignItems:"center",justifyContent:"center",flexDirection:"column",padding:"10px" }}>
             <Typography variant="h2" gutterBottom>
                   TodoList
             </Typography>
-            <hr/>
             <CardContent style={{display:"flex",justifyContent:"center",alignItems:"center",padding:"10px"}}>
               {/* input filed and add button */}
               <TextField style={{margin:"10px"}} id="outlined-basic" label="Title" variant="outlined" /> 
@@ -22,6 +22,8 @@ export default function MyTodos() {
               <Button style={{margin:"5px",height:"45px"}} variant="contained">Add</Button>
                {/*==== input filed and add button ====*/}
             </CardContent>
+            <hr/>
+            <Todos/> 
          </Card> 
       </Container>
     </React.Fragment>
